0266faf4ea10c1246d3b3e1c4790727a832658fa
José Barroso <jose.renato77@gmail.com>
2023-10-29 22:57:33 +0000

    Add refresh token and improve coverage

--- tests/test_users.py
+++ tests/test_users.py
@@ -58,7 +58,7 @@ def test_update_user(client, user, token):
     assert response.json() == {
         'username': 'jose',
         'email': 'jose@example.com',
-        'id': 1,
+        'id': user.id,
     }
 
 
@@ -76,9 +76,9 @@ def test_update_user_not_credentials(client):
     assert response.json() == {'detail': 'Not authenticated'}
 
 
-def test_update_user_not_same_user(client, token):
+def test_update_user_not_same_user(client, other_user, token):
     response = client.put(
-        '/users/4',
+        f'/users/{other_user.id}',
         headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'jose',
@@ -108,9 +108,9 @@ def test_delete_user_fail_not_credentials(client):
     assert response.json() == {'detail': 'Not authenticated'}
 
 
-def test_delete_user_fail_not_same_user(client, token):
+def test_delete_user_fail_not_same_user(client, other_user, token):
     response = client.delete(
-        '/users/3', headers={'Authorization': f'Bearer {token}'}
+        f'/users/{other_user.id}', headers={'Authorization': f'Bearer {token}'}
     )
 
     assert response.status_code == 400
